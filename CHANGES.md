85ba85947e2c6c671b1ab91182f7b363ca0ef0ca
Jordi Pujol Ahulló <jpahullo@gmail.com>
2021-08-11 08:34:26 +0200

    #3 - use github actions to pass tests and release version
     #3 - reduce CI effort

---
+++ CHANGES.md
@@ -0,0 +1,13 @@
+Release notes
+=============
+
+1. Keep minimum Moodle version supported as 2.5.
+1. (#3) Use Github Actions to pass tests and update release on Moodle Plugins directory.
+
+Contributors
+============
+
+Maintained by:
+* Jordi Pujol-Ahulló (at SREd, Universitat Rovira i Virgili).
+
+[See all Github contributors](https://github.com/SREd-URV/moodle-profilefield_timestamp/graphs/contributors)
--- version.php
+++ version.php
@@ -26,8 +26,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2013031400;        // The current plugin version (Date: YYYYMMDDXX)
-$plugin->requires  = 2013051400;        // Requires this Moodle version (2.5)
+$plugin->version   = 2021081100;               // The current plugin version (Date: YYYYMMDDXX)
+$plugin->requires  = 2013051400;               // Requires this Moodle version (2.5)
 $plugin->component = 'profilefield_timestamp'; // Full name of the plugin (used for diagnostics)
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '1.0 (Build: 2013101800)'; 
+$plugin->release   = '2021081100';
